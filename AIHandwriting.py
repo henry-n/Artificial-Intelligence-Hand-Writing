e5f17ddf9ca9882c16f9e3ec707d559dfec2c799
SpeedOfSmell <vldmirea@gmail.com>
2018-03-30 20:49:37 -0400

    Fixed image() function and added code to print the images

--- AIHandwriting.py
+++ AIHandwriting.py
@@ -1,6 +1,5 @@
 import scipy.io
 import numpy
-import binascii
 
 # Structure of image files
 # [offset] [type]          [value]          [description] 
@@ -19,14 +18,38 @@ def image():
     itemsNum = int.from_bytes(data.read(4), "big")
     imageSize = int.from_bytes(data.read(4), byteorder='big'), int.from_bytes(data.read(4), byteorder='big')
 
-    images = []
-    for image in range(itemsNum): # Iterate every pixel
-        imageMatrix = numpy.zeros((imageSize[0], imageSize[1])) # Create a matrix to hold all the pixels of a single image
-        for j in range(imageSize[1]): # Iterate through every spot in the matrix and save each pixel in the right spot
-            for i in range(imageSize[0]):
-                imageMatrix[i][j] = int.from_bytes(data.read(1), byteorder='big') # Add the matrix to the list containing the images
+    # I don't know wtf I was thinking when I wrote this
+    #images = []
+    #for image in range(int(itemsNum / 4)): # Iterate every pixel
+    #    imageMatrix = numpy.zeros((imageSize[0], imageSize[1])) # Create a matrix to hold all the pixels of a single image
+    #    for j in range(imageSize[1]): # Iterate through every spot in the matrix and save each pixel in the right spot
+    #        for i in range(imageSize[0]):
+    #            imageMatrix[i][j] = int.from_bytes(data.read(1), byteorder='big') # Add the matrix to the list containing the images
 
-        images.append(imageMatrix)
+    #    images.append(imageMatrix)
+
+    images = []
+    file = open("images.txt", "w") # Image matrices will be printed to this file
+    imageMatrix = numpy.zeros((imageSize[0], imageSize[1]), dtype = int) # Create a matrix to hold all the pixels of a single image
+    imageMatrix = imageMatrix.flatten()
+    for index in range(int(itemsNum / 4)): # Iterate every pixel
+        imageMatrix[index % (imageSize[0] * imageSize[1])] = int.from_bytes(data.read(1), byteorder='big') # Read each pixel, byte by byte
+
+        if ((index + 1) % (imageSize[0] * imageSize[1]) == 0): # Check if all pixels in one image have been processed
+            imageMatrix = imageMatrix.reshape((imageSize[0], imageSize[1])) # Reshape the matrix
+            images.append(imageMatrix) # Add the matrix to the list
+
+            for i in range(imageSize[0]): # Print the matrix to the file
+                s = ''
+                for j in range(imageSize[1]):
+                    s+=('{:03d}' + ' ').format(int(imageMatrix[i][j]))
+                s += "\n"
+                file.write(s);
+            file.write("\n\n")
+
+            imageMatrix = numpy.zeros((imageSize[0], imageSize[1])) # Create a new matrix
+            imageMatrix = imageMatrix.flatten()
+    
 
 # Structure of label files
 # [offset] [type]          [value]          [description] 
@@ -36,7 +59,7 @@ def image():
 # 0009     unsigned byte   ??               label 
 # ........ 
 # xxxx     unsigned byte   ??               label
-# The labels values are 1 to .
+# The labels values are 1 to 26 (it seems like)
 def label():
     data = open("Datasets\emnist-letters-train-labels-idx1-ubyte", "rb")
     magicNum = data.read(4)
@@ -44,5 +67,7 @@ def label():
 
     labels = []
     for label in range(itemsNum):
-        char = chr(int.from_bytes(data.read(1), byteorder='big') + 64)
+        char = chr(int.from_bytes(data.read(1), byteorder='big') + 64) # Idk if I'm interpreting this right
         labels.append(char)
+
+image()
